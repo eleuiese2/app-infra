47465810aeb2b1eed048ec52305f48cb43a29290
lestra2 <lestra2@bancodebogota.com.co>
2025-04-06 23:56:16 -0500

    Test VPC

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -1,7 +1,10 @@
 module "vpc_virginia" {
   source     = "github.com/eleuiese2/aws-vpc-iac.git"
-  region     = "us-east-1"
-  cidr_block = var.cidr_block
-  azs        = var.azs
-  tags       = var.tags
+  create               = var.create
+  vpc_name             = var.vpc_name
+  cidr_block_vpc       = var.cidr_block_vpc
+  region               = var.region
+  cidr_public_subnets  = var.cidr_public_subnets
+  cidr_private_subnets = var.cidr_private_subnets
+  availability_zones   = var.availability_zones
 }
