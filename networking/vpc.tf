a729bfc428308f5181ade7ac27b8d8234364c7f1
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 00:10:46 -0500

    Test VPC

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -1,5 +1,8 @@
 module "vpc_virginia" {
-  source               = "github.com/eleuiese2/aws-vpc-iac.git"
+  source = "github.com/eleuiese2/aws-vpc-iac.git"
+  providers = {
+    aws = aws.virginia
+  }
   create               = var.create
   vpc_name             = var.vpc_name
   cidr_block_vpc       = var.cidr_block_vpc
