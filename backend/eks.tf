01de29335aa321895f09e8d48475cef2541a4b3f
lestra2 <lestra2@bancodebogota.com.co>
2025-04-08 01:28:48 -0500

    test vpc and eks

--- backend/eks.tf
+++ backend/eks.tf
@@ -3,10 +3,10 @@ module "eks_virginia" {
   create                         = var.create_eks
   create_ecr                     = var.create_ecr
   ecr_name                       = var.ecr_name
+  namespace                      = var.namespace
   vpc_id                         = var.vpc_id
   public_subnet_ids              = var.public_subnet_ids
   private_subnet_ids             = var.private_subnet_ids
-  environment                    = var.environment
   kubernetes_version             = var.kubernetes_version
   tags                           = var.tags
   allowed_ip                     = var.allowed_ip
