7839465b1b4f4bfef9fccec0b6c8453cd449d6a3
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 23:03:00 -0500

    test vpc and eks

--- backend/eks.tf
+++ backend/eks.tf
@@ -2,6 +2,7 @@ module "eks_virginia" {
   source             = "github.com/eleuiese2/aws-eks-iac.git"
   create             = var.create_eks
   create_ecr         = var.create_ecr
+  ecr_name           = var.ecr_name
   vpc_id             = var.vpc_id
   project            = var.project
   namespace          = var.namespace
