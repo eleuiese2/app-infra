43b9926390f0482dcd85b44cc5da7ca234bfb084
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 19:47:35 -0500

    test vpc and eks

--- backend/eks.tf
+++ backend/eks.tf
@@ -1,6 +1,6 @@
 module "eks_virginia" {
   source               = "github.com/eleuiese2/aws-eks-iac.git"
-  create               = var.create
+  create               = var.create_eks
   vpc_id               = var.vpc_id
   cidr_block_vpc       = var.cidr_block_vpc
   region               = var.region
