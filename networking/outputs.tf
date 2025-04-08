474c9bbb9beb379a88a67e1cc71c59e5d7ee7013
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 21:02:20 -0500

    test vpc and eks

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -3,13 +3,13 @@ output "vpc_id" {
   value       = module.vpc_virginia.vpc_id
 }
 
-output "public_subnets_ids" {
+output "public_subnet_ids" {
   description = "The IDs of the public subnets"
   value       = module.vpc_virginia.public_subnets_ids
 
 }
 
-output "private_subnets_ids" {
+output "private_subnet_ids" {
   description = "The IDs of the private subnets"
   value       = module.vpc_virginia.private_subnets_ids
 }
