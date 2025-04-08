ca5519ed1359003256c17adb54c75a1317b9b8ac
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 20:17:37 -0500

    test vpc and eks

--- backend/eks.tf
+++ backend/eks.tf
@@ -2,11 +2,6 @@ module "eks_virginia" {
   source               = "github.com/eleuiese2/aws-eks-iac.git"
   create               = var.create_eks
   vpc_id               = var.vpc_id
-  cidr_block_vpc       = var.cidr_block_vpc
-  region               = var.region
-  cidr_public_subnets  = var.cidr_public_subnets
-  cidr_private_subnets = var.cidr_private_subnets
-  availability_zones   = var.availability_zones
   project              = var.project
   namespace            = var.namespace
   environment          = var.environment
