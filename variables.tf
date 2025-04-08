6fdce38379826dbce160fc38c69c467949bee075
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 21:13:43 -0500

    test vpc and eks

--- variables.tf
+++ variables.tf
@@ -59,14 +59,6 @@ variable "project" {
   type = string
 }
 
-variable "vpc_id" {
-  type = string
-}
-
-variable "subnet_ids" {
-  type = list(string)
-}
-
 variable "kubernetes_version" {
   type = string
 }
